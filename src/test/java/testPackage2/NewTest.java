b7f90f17efcb466771205262a8f170b1f4f9cdc6
Dell <Dell@DESKTOP-TH2S411>
2021-06-19 11:31:41 -0500

    Inicial commit

---
+++ src/test/java/testPackage2/NewTest.java
@@ -0,0 +1,11 @@
+package testPackage2;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  Assert.assertTrue(false);
+  }
+}
